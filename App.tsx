f1d083cfe735e91d2f32a27fa5151d7a4f814c0d
Flemming Eriksson <fleeri91@gmail.com>
2024-11-06 13:38:56 +0100

    wip

--- App.tsx
+++ App.tsx
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import OnboardingView from 'screens/OnboardingView'
+import { useGeoStore } from 'store/useGeo'
 
 const App = () => {
+  const { geoLocation } = useGeoStore()
+
+  if (!geoLocation) {
+    return <OnboardingView />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+        style={styles.mapView}
+        initialRegion={{
+          latitude: geoLocation.coords.latitude,
+          longitude: geoLocation.coords.longitude,
+          latitudeDelta: 0.01, // Zoom level, adjust as needed
+          longitudeDelta: 0.01, // Zoom level, adjust as needed
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: geoLocation.coords.latitude,
+            longitude: geoLocation.coords.longitude,
+          }}
+          title="You are here"
+          description="Your current location"
+        />
+      </MapView>
     </View>
   )
 }
+
 export default App
 
 const styles = StyleSheet.create({
@@ -17,5 +44,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapView: {
+    height: '100%',
+    width: '100%',
   },
 })
