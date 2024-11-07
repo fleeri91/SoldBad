7951acb9f074af512146ecaaf16a70e49c8f2c93
Flemming <fleeri91@gmail.com>
2024-11-07 01:01:54 +0100

    current location

--- src/store/usePreferences.ts
+++ src/store/usePreferences.ts
@@ -1,17 +1,29 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { GooglePlaceData, GooglePlaceDetail } from 'react-native-google-places-autocomplete'
 
-type PreferencesState = {}
+type PreferencesState = {
+  currentLocation: {
+    data: GooglePlaceData | null
+    details: GooglePlaceDetail | null
+  }
+}
 
-type PreferencesActions = {}
+type PreferencesActions = {
+  setCurrentLocation: (data: GooglePlaceData, details: GooglePlaceDetail | null) => void
+}
 
-const initialState: PreferencesState = {}
+const initialState: PreferencesState = {
+  currentLocation: { data: null, details: null },
+}
 
 export const usePreferencesStore = create<PreferencesState & PreferencesActions>()(
   persist(
     (set) => ({
       ...initialState,
+      setCurrentLocation: (data, details) =>
+        set({ currentLocation: { data: data, details: details } }),
     }),
     {
       name: 'preferences-storage',
