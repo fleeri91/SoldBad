ac873171ce22222c36330cfc493e5be02b47d6a1
Flemming <fleeri91@gmail.com>
2024-11-07 17:41:42 +0100

    update

--- App.tsx
+++ App.tsx
@@ -1,25 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import 'react-native-get-random-values'
+import 'react-native-gesture-handler'
+
+import MainNavigation from 'navigation/MainNavigation'
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
-  )
+  return <MainNavigation />
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapView: {
-    height: '100%',
-    width: '100%',
-  },
-})
