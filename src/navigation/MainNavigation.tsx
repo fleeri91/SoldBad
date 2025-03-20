716fb870783209bd7ba417641ad9b0a53770d70a
Flemming <fleeri91@gmail.com>
2025-03-20 18:27:58 +0100

    tab nav

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { useColorScheme } from 'react-native'
-
-import { CustomLightTheme, CustomDarkTheme } from 'src/theme/colors'
 
 import DebugScreen from 'screens/DebugScreen'
 import OnboardingScreen from 'screens/OnboardingScreen'
-import MapScreen from 'screens/MapScreen'
+import MapScreenCluster from 'screens/tabs/MapScreenCluster'
+import MapScreen from 'screens/tabs/MapScreen'
 import AllowPermissionScreen from 'screens/AllowPermissionScreen'
 
 import { useGeoStore } from 'store/useGeo'
 import { useSettingsStore } from 'store/useSettings'
 import { IS_DEBUGGING } from 'app-settings'
+import ListScreen from 'screens/tabs/ListScreen'
+import { darkTheme, lightTheme } from 'src/theme'
 
 export type RootStackParamList = {
   Debug: undefined
   AllowPermission: undefined
   Onboarding: undefined
-  Map: undefined
+  Home: undefined
 }
 
 const Stack = createStackNavigator<RootStackParamList>()
+const Tab = createBottomTabNavigator()
 
 const MainNavigation = () => {
   const { geoLocation } = useGeoStore()
@@ -29,14 +32,15 @@ const MainNavigation = () => {
 
   const { theme } = useSettingsStore()
 
-  const appTheme = (theme ?? colorScheme === 'dark') ? CustomDarkTheme : CustomLightTheme
+  const appTheme = (theme ?? colorScheme === 'dark') ? darkTheme : lightTheme
 
   return (
     <NavigationContainer theme={appTheme}>
       <Stack.Navigator
-        initialRouteName={IS_DEBUGGING ? 'Debug' : !geoLocation ? 'Onboarding' : 'AllowPermission'}
+        initialRouteName={IS_DEBUGGING ? 'Debug' : !geoLocation ? 'Onboarding' : 'Home'}
       >
         <Stack.Screen name="Debug" options={{ headerShown: false }} component={DebugScreen} />
+        {/*
         <Stack.Screen
           name="AllowPermission"
           options={{ headerShown: false }}
@@ -47,9 +51,19 @@ const MainNavigation = () => {
           options={{ headerShown: false }}
           component={OnboardingScreen}
         />
-        <Stack.Screen name="Map" options={{ headerShown: false }} component={MapScreen} />
+        */}
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
+  )
+}
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Map" component={MapScreen} options={{ title: 'Karta' }} />
+      <Tab.Screen name="List" component={ListScreen} options={{ title: 'Lista' }} />
+    </Tab.Navigator>
   )
 }
 
