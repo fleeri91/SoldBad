aa5834862c3370947c275f25348cc8da6c11d17a
Flemming Eriksson <fleeri91@gmail.com>
2025-03-21 00:46:07 +0100

    tanstack query

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import 'react-native-get-random-values'
 import 'react-native-gesture-handler'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import MainNavigation from 'navigation/MainNavigation'
 
+const queryClient = new QueryClient()
+
 const App = () => {
-  return <MainNavigation />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainNavigation />
+    </QueryClientProvider>
+  )
 }
 
 export default App
