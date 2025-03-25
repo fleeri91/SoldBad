781e9ab24b109e6d9b63db5f4d57bbb1903195a9
Flemming Eriksson <fleeri91@gmail.com>
2025-03-26 00:53:35 +0100

    Bathing water info fetch

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,6 @@
 import { BathingWaterRoot, BathingWater } from 'types/BathingWater'
+import { BathingWaterInfoRoot } from 'types/BathingWaterInfo'
+
 import { WaterTypeId } from 'types/WaterType'
 import { delay } from 'utils/helpers'
 
@@ -22,5 +24,26 @@ export const fetchBathingWaters = async (): Promise<BathingWater[]> => {
     .catch((error) => {
       console.error('Failed to fetch bathing waters:', error)
       return Promise.reject('Failed to load bathing waters')
+    })
+}
+
+export const fetchBathingWaterInfo = async (id: string): Promise<BathingWaterInfoRoot> => {
+  console.log(`Fetching bathing water ${id}...`)
+
+  await delay(200 + Math.floor(Math.random() * 2000))
+
+  return fetch(
+    `https://gw-test.havochvatten.se/external-public/bathing-waters/v1/bathing-water-profiles/${id}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject('Failed to fetch data')
+      }
+      return response.json()
+    })
+    .then()
+    .catch((error) => {
+      console.error(`Failed to fetch bathing water with id ${id}: ${error}`)
+      return Promise.reject(`Failed to load bathing water with id ${id}`)
     })
 }
