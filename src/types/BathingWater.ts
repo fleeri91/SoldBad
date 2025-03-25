57a7c4f1733f2420a98919b820c8b5af95e76ee9
Flemming Eriksson <fleeri91@gmail.com>
2025-03-26 00:17:38 +0100

    rename

---
+++ src/types/BathingWater.ts
@@ -0,0 +1,48 @@
+export type BathingWaterRoot = BathingWater[]
+
+export interface BathingWater {
+  adviceAgainstBathing: AdviceAgainstBathing[]
+  bathingWater: BathingWater
+}
+
+export interface AdviceAgainstBathing {
+  description: string
+  startsAt: string
+  typeId: number
+  typeIdText: string
+}
+
+export interface BathingWater {
+  description?: string
+  euMotive?: string
+  euType: boolean
+  municipality: Municipality
+  name: string
+  notEuMotive: string
+  nutsCode: string
+  perimeterCoordinates: PerimeterCoordinate[]
+  samplingPointPosition: SamplingPointPosition
+  waterTypeId: number
+  waterTypeIdText: string
+}
+
+export interface Municipality {
+  contactInfo: ContactInfo
+  name: string
+}
+
+export interface ContactInfo {
+  email?: string
+  phone?: string
+  url?: string
+}
+
+export interface PerimeterCoordinate {
+  latitude: string
+  longitude: string
+}
+
+export interface SamplingPointPosition {
+  latitude: string
+  longitude: string
+}
