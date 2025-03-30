e681e1d37006b44a28fa4ef250e12174a637547f
Flemming Eriksson <fleeri91@gmail.com>
2025-03-30 16:46:26 +0200

    Error screen

---
+++ src/screens/ErrorScreen/ErrorScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+
+const ErrorScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Error screen</Text>
+    </View>
+  )
+}
+
+export default ErrorScreen
