2d55a094a3a6a2543280d35836ef9b1914722ea1
Flemming Eriksson <fleeri91@gmail.com>
2025-03-30 16:46:46 +0200

    Error boundary

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import 'react-native-get-random-values'
 import 'react-native-gesture-handler'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import MainNavigation from 'navigation/MainNavigation'
+import ErrorScreen from 'screens/ErrorScreen'
 
 const queryClient = new QueryClient()
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainNavigation />
+      <ErrorBoundary FallbackComponent={ErrorScreen}>
+        <MainNavigation />
+      </ErrorBoundary>
     </QueryClientProvider>
   )
 }
